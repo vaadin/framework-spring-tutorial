57fb2bca2835a3093c14660f72826f606493d763
Teppo Kurki <teppo.kurki@vaadin.com>
2015-05-28 14:45:01 +0300

    II - Injection and Scopes with Vaadin Spring

--- src/main/java/org/vaadin/spring/tutorial/MyVaadinUI.java
+++ src/main/java/org/vaadin/spring/tutorial/MyVaadinUI.java
@@ -1,14 +1,21 @@
 package org.vaadin.spring.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
+@Theme("valo")
 @SpringUI
 public class MyVaadinUI extends UI {
+    @Autowired
+    private Greeter greeter;
+
     @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! I'm the root UI!"));
+    protected void init(VaadinRequest request) {
+        setContent(new Label(greeter.sayHello()));
     }
 }
