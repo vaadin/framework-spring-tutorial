4fa134ce88738342a9c1d360b2b0699c053d0680
Henri Sara <hesara@vaadin.com>
2016-10-26 16:18:35 +0300

    I - Getting Started with Vaadin Spring and Spring Boot

---
+++ src/main/java/org/vaadin/spring/tutorial/MyVaadinUI.java
@@ -0,0 +1,14 @@
+package org.vaadin.spring.tutorial;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class MyVaadinUI extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setContent(new Label("Hello! I'm the root UI!"));
+    }
+}
