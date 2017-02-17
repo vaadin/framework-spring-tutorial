5e08e308d70fe726e1cf42a474ce3e480d6caacd
elmot <elmotelmot.vaadin.com>
2017-02-17 17:25:07 +0200

    Upgrade to Vaadin 8 and vaadin-spring 2.0

--- src/test/java/org/vaadin/spring/tutorial/TutorialApplicationTests.java
+++ src/test/java/org/vaadin/spring/tutorial/TutorialApplicationTests.java
@@ -2,13 +2,12 @@ package org.vaadin.spring.tutorial;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.vaadin.spring.tutorial.TutorialApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TutorialApplication.class)
+@SpringBootTest(classes = TutorialApplication.class)
 @WebAppConfiguration
 public class TutorialApplicationTests {
 
